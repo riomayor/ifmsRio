5d940d18a4b2c6c4bf5d0bee4f687cfb7b94ac64
LARA RODRIGUEZ RIOMAYOR TnolADS <16131905738@laboratorio.ifms.br>
2025-04-01 08:41:39 -0400

    jogando

---
+++ app/Http/Controllers/CoordenadorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coordenador;
+use Illuminate\Http\Request;
+
+class CoordenadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Coordenador $coordenador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Coordenador $coordenador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Coordenador $coordenador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Coordenador $coordenador)
+    {
+        //
+    }
+}
